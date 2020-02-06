cbdc7d7267a9f66f2f89906ccb3e80fb3574b49a
Hernán Di Pietro <hernan.di.pietro@gmail.com>
2020-02-06 17:26:12 -0300

    Recursive menu builder now.

--- xemu/gui/macos.c
+++ xemu/gui/macos.c
@@ -31,24 +31,73 @@ typedef id   (* PFN_OBJC_MSGSEND_IDV)(id, SEL);
 typedef void (* PFN_OBJC_MSGSEND_VID)(id, SEL, id);
 typedef id   (* PFN_OBJC_MSGSEND_IDID)(id, SEL, id);
 typedef void (* PFN_OBJC_MSGSEND_VBOOL)(id, SEL, BOOL);
+typedef void (* PFN_OBJC_MSGSEND_VID2)(id, SEL, id, id);
 typedef void (* PFN_OBJC_MSGSEND_VID3)(id, SEL, id, id, id);
 typedef BOOL (* PFN_OBJC_MSGSEND_BOOLID3)(id, SEL, id, id, id);
 typedef BOOL (* PFN_OBJC_MSGSEND_BOOLIDSELID)(id, SEL, id, SEL, id);
-typedef id   (* PFN_OBJC_MSGSEND_IDSTR)(id, SEL, char*);
+typedef id   (* PFN_OBJC_MSGSEND_IDIDSELID)(id, SEL, id, SEL, id);
+typedef id   (* PFN_OBJC_MSGSEND_IDSTR)(id, SEL, const char*);
 
 static PFN_OBJC_MSGSEND_VV  		pfn_objc_msgsend_vv  = 		(PFN_OBJC_MSGSEND_VV)  objc_msgSend; 
 static PFN_OBJC_MSGSEND_IDV 		pfn_objc_msgsend_idv = 		(PFN_OBJC_MSGSEND_IDV) objc_msgSend; 
 static PFN_OBJC_MSGSEND_VID 		pfn_objc_msgsend_vid = 		(PFN_OBJC_MSGSEND_VID) objc_msgSend;
 static PFN_OBJC_MSGSEND_IDID 		pfn_objc_msgsend_idid = 	(PFN_OBJC_MSGSEND_IDID) objc_msgSend;
 static PFN_OBJC_MSGSEND_VBOOL 		pfn_objc_msgsend_vbool = 	(PFN_OBJC_MSGSEND_VBOOL) objc_msgSend;
-static PFN_OBJC_MSGSEND_VID3 		pfn_objc_msgsend_vid3 =		(PFN_OBJC_MSGSEND_VID3)objc_msgSend;
+static PFN_OBJC_MSGSEND_VID2 		pfn_objc_msgsend_vid2 =		(PFN_OBJC_MSGSEND_VID2)  objc_msgSend;
+static PFN_OBJC_MSGSEND_VID3 		pfn_objc_msgsend_vid3 =		(PFN_OBJC_MSGSEND_VID3)  objc_msgSend;
 static PFN_OBJC_MSGSEND_BOOLID3 	pfn_objc_msgsend_bid3 =		(PFN_OBJC_MSGSEND_BOOLID3)objc_msgSend;
 static PFN_OBJC_MSGSEND_BOOLIDSELID pfn_objc_msgsend_bidselid = (PFN_OBJC_MSGSEND_BOOLIDSELID)objc_msgSend;
+static PFN_OBJC_MSGSEND_IDIDSELID   pfn_objc_msgsend_ididselid =(PFN_OBJC_MSGSEND_IDIDSELID)objc_msgSend;
 static PFN_OBJC_MSGSEND_IDSTR 		pfn_objc_msgsend_idstr = 	(PFN_OBJC_MSGSEND_IDSTR) objc_msgSend;
 
 static void _xemumacgui_menu_action_handler(id self, SEL selector, id sender) 
 {
+	id menu_obj = pfn_objc_msgsend_idv(sender, sel_registerName("representedObject"));
+	const struct menu_st* menu_item = (const struct menu_st*) pfn_objc_msgsend_idv(menu_obj, sel_registerName("pointerValue"));
+	if (menu_item && menu_item->type == XEMUGUI_MENUID_CALLABLE)
+	{
+		DEBUGPRINT("GUI: menu point \"%s\" has been activated." NL,  menu_item->name);
+		((xemugui_callback_t)(menu_item->handler))(menu_item, NULL);
+	}
+}
 
+static id _xemumacgui_r_menu_builder(const struct menu_st desc[])
+{
+	id ui_menu = pfn_objc_msgsend_idv((id)objc_getClass("NSMenu"), sel_registerName("new"));
+	pfn_objc_msgsend_vv(ui_menu, sel_registerName("autorelease"));
+
+	for (int i = 0; desc[i].name; i++) 
+	{
+		if (!desc[i].handler || !desc[i].name) {
+			DEBUGPRINT("GUI: invalid meny entry found, skipping it" NL);
+			continue;
+		}
+
+		id menu_item = pfn_objc_msgsend_idv((id) objc_getClass("NSMenuItem"), sel_registerName("alloc"));
+		pfn_objc_msgsend_vv(menu_item, sel_registerName("autorelease"));
+
+		pfn_objc_msgsend_ididselid(menu_item, sel_registerName("initWithTitle:action:keyEquivalent:"),  
+			pfn_objc_msgsend_idstr((id) objc_getClass("NSString"), sel_registerName("stringWithUTF8String:"), desc[i].name),
+        	sel_registerName("menuActionHandler"),
+        	pfn_objc_msgsend_idstr((id) objc_getClass("NSString"), sel_registerName("stringWithUTF8String:"), ""));
+
+		pfn_objc_msgsend_vbool(menu_item, sel_registerName("setEnabled:"), YES);
+
+		id menu_object = pfn_objc_msgsend_idid((id)objc_getClass("NSValue"), sel_registerName("valueWithPointer:"),(id) &desc[i]);
+		pfn_objc_msgsend_vid(menu_item, sel_registerName("setRepresentedObject:"), menu_object);
+
+		pfn_objc_msgsend_vid(ui_menu, sel_registerName("addItem:"), menu_item);
+
+		if (desc[i].type == XEMUGUI_MENUID_SUBMENU)
+		{
+			// Recurse
+
+			id sub_menu = _xemumacgui_r_menu_builder(desc[i].handler);
+			pfn_objc_msgsend_vid2(ui_menu, sel_registerName("setSubmenu:forItem:"), sub_menu, menu_item);
+		}
+	}
+
+	return ui_menu;
 }
 
 static int xemumacgui_init(void) 
@@ -71,49 +120,35 @@ static int xemumacgui_init(void)
 	application = pfn_objc_msgsend_idv((id)objc_getClass("NSApplication"), sel_registerName("sharedApplication"));
 	pfn_objc_msgsend_vid(application, sel_registerName("setDelegate:"), xemu_ui_delegate);
 
-	return 1;
+	return 0;
 }
 
 static int xemumacgui_popup(const struct menu_st desc[]) 
-{
-	id ui_menu = pfn_objc_msgsend_idv((id)objc_getClass("NSMenu"), sel_registerName("new"));
-	pfn_objc_msgsend_vv(ui_menu, sel_registerName("autorelease"));
-
-	for (int i = 0; desc[i].name; i++) 
+{ 
+	id ui_menu = _xemumacgui_r_menu_builder(desc);
+	if (!ui_menu)
 	{
-		id menu_item = pfn_objc_msgsend_idv(objc_getClass("NSMenuItem"), sel_registerName("alloc"));
-
-		pfn_objc_msgsend_vid3(menu_item, sel_registerName("initWithTitle:action:keyEquivalent:"),  
-			pfn_objc_msgsend_idstr(objc_getClass("NSString"), sel_registerName("stringWithUTF8String:"), desc[i].name),
-        	sel_registerName("menuActionHandler"),
-        	pfn_objc_msgsend_idstr(objc_getClass("NSString"), sel_registerName("stringWithUTF8String:"), ""));
-
-		pfn_objc_msgsend_vbool(menu_item, sel_registerName("setEnabled:"), YES);
-
-		id menu_object = pfn_objc_msgsend_idid((id)objc_getClass("NSValue"), sel_registerName("valueWithPointer:"), &desc[i]);
-		pfn_objc_msgsend_vid(menu_item, sel_registerName("setRepresentedObject:"), menu_object);
-
-		pfn_objc_msgsend_vid(ui_menu, sel_registerName("addItem:"), menu_item);
+		DEBUGPRINT("GUI: Error building menu");
+		return 1;
 	}
 
 	id mouse_location = pfn_objc_msgsend_idv((id)objc_getClass("NSEvent"), sel_registerName("mouseLocation"));
-	
-	BOOL s = pfn_objc_msgsend_bid3(ui_menu, 
-		sel_registerName("popUpMenuPositioningItem:atLocation:inView:"), nil, (id) mouse_location, nil);
+	pfn_objc_msgsend_bid3(ui_menu, sel_registerName("popUpMenuPositioningItem:atLocation:inView:"), nil, (id) mouse_location, nil);
 
     return 0;
 }
 
 static int xemumacgui_file_selector(void) 
 {
+	return 0;
 }
 
 static const struct xemugui_descriptor_st xemumacosgui_descriptor = {
 	"macos",					// name
-	"macOS API based Xemu UI implementation",	// desc
+	"macOS native API Xemu UI implementation",	// desc
 	xemumacgui_init,
-	NULL,						// shutdown (we don't need shutdown for windows?)
-	NULL,						// iteration (we don't need iteration for windows?)
+	NULL,						
+	NULL,						
 	xemumacgui_file_selector,
 	xemumacgui_popup
 };
